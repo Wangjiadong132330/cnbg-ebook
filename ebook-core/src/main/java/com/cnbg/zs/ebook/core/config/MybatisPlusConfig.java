18c603fb1bb71ba2bf16e12fbb38c5b91999f370
vikenlove <wangfei1321@gmail.com>
2021-04-07 15:54:13 +0800

    fixed return pages value is 0

---
+++ ebook-core/src/main/java/com/cnbg/zs/ebook/core/config/MybatisPlusConfig.java
@@ -0,0 +1,41 @@
+package com.cnbg.zs.ebook.core.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.ey.gds.voc.core.handler.MyMetaObjectHandler;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Faye.Wang
+ * @version 1.0
+ * @date 2021/1/12 18:14
+ * @Description
+ */
+@Configuration
+@MapperScan("com.cnbg.zs.ebook.api.mapper")
+public class MybatisPlusConfig {
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setUseDeprecatedExecutor(false);
+    }
+
+    @Bean
+    public MyMetaObjectHandler myMetaObjectHandler () {
+        return new MyMetaObjectHandler ();
+    }
+
+}
