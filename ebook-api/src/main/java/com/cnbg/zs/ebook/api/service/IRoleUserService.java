5a3de7c206e095fb79c073ea4d030c74f2b9cd9a
vikenlove <wangfei1321@gmail.com>
2021-04-08 09:29:28 +0800

    fixed return pages value is 0

--- ebook-api/src/main/java/com/cnbg/zs/ebook/api/service/IRoleUserService.java
+++ ebook-api/src/main/java/com/cnbg/zs/ebook/api/service/IRoleUserService.java
@@ -1,28 +1,32 @@
 package com.cnbg.zs.ebook.api.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cnbg.zs.ebook.api.entity.Permission;
 import com.cnbg.zs.ebook.api.entity.RoleUser;
 
 import java.util.List;
 
 /**
- * @author Faye.Wang
- * @version 1.0
- * @date 2021/3/31 10:52
- * @Description
- */
+* @author Faye.Wang
+* @version 1.0
+* @date 2021/1/6 14:49
+* @Description
+*/
 public interface IRoleUserService {
-    /**
-     * 查询用户当前的角色列表
-     * @param userId
-     * @return
-     */
-    List<RoleUser> getRoleUserForUser(Integer userId);
+	/**
+	 * 查询用户当前的角色列表
+	 * @param userId
+	 * @return
+	 */
+	List<RoleUser> getRoleUserForUser(Integer userId);
 
-    /**
-     * 查询用户访问权限URL
-     * @param userId
-     * @return
-     */
-    List<Permission> getPermissionForUser(Integer userId);
+	/**
+	 * 查询用户访问权限URL
+	 * @param userId
+	 * @return
+	 */
+	List<Permission> getPermissionForUser(Integer userId);
+
+
 }
--- ebook-api/src/main/java/com/cnbg/zs/ebook/api/service/impl/IRoleUserServiceImpl.java
+++ ebook-api/src/main/java/com/cnbg/zs/ebook/api/service/impl/IRoleUserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.cnbg.zs.ebook.api.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cnbg.zs.ebook.api.entity.Permission;
 import com.cnbg.zs.ebook.api.entity.RoleUser;
 import com.cnbg.zs.ebook.api.mapper.PermissionMapper;
@@ -8,34 +10,38 @@ import com.cnbg.zs.ebook.api.mapper.RoleUserMapper;
 import com.cnbg.zs.ebook.api.service.IRoleUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.*;
 
 /**
- * @author Faye.Wang
- * @version 1.0
- * @date 2021/3/31 10:53
- * @Description
- */
+* @author OFG
+* @version 1.0
+* @date 2021/1/6 20:09
+* @Description
+*/
 @Service
 public class IRoleUserServiceImpl implements IRoleUserService {
-    @Autowired
-    private RoleUserMapper roleUserMapper;
-    @Autowired
-    private PermissionMapper permissionMapper;
 
-    @Override
-    public List<RoleUser> getRoleUserForUser(Integer userId) {
-        // 获取用户角色
-        QueryWrapper<RoleUser> queryWrapper2 = new QueryWrapper<>();
-        queryWrapper2.eq("user_id",userId);
-        List<RoleUser> roleUsers = roleUserMapper.selectList(queryWrapper2);
-        return roleUsers;
-    }
+	@Autowired
+	private RoleUserMapper roleUserMapper;
+	@Autowired
+	private PermissionMapper permissionMapper;
 
-    @Override
-    public List<Permission> getPermissionForUser(Integer userId) {
-        return permissionMapper.getPermissionByUserId(userId);
-    }
+	@Override
+	public List<RoleUser> getRoleUserForUser(Integer userId) {
+		// 获取用户角色
+		QueryWrapper<RoleUser> queryWrapper2 = new QueryWrapper<>();
+		queryWrapper2.eq("user_id",userId);
+		List<RoleUser> roleUsers = roleUserMapper.selectList(queryWrapper2);
+		return roleUsers;
+	}
+
+	@Override
+	public List<Permission> getPermissionForUser(Integer userId) {
+		return permissionMapper.getPermissionByUserId(userId);
+	}
+
 
 }
